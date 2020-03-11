eda9bb7e81dc9dc7e63f23e6332858e322786235
xianny <xianny@gmail.com>
2020-03-10 18:35:16 -0700

    gitpkg

---
+++ lib/src/generated-wrappers/lib_transaction_decoder.d.ts
@@ -0,0 +1,52 @@
+import { ContractFunctionObj, BaseContract } from '@0x/base-contract';
+import { ContractAbi, ContractArtifact, TxData, SupportedProvider } from 'ethereum-types';
+import { BigNumber } from '@0x/utils';
+import { SimpleContractArtifact } from '@0x/types';
+export declare class LibTransactionDecoderContract extends BaseContract {
+    /**
+     * @ignore
+     */
+    static deployedBytecode: string;
+    static contractName: string;
+    private readonly _methodABIIndex;
+    static deployFrom0xArtifactAsync(artifact: ContractArtifact | SimpleContractArtifact, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: ContractArtifact | SimpleContractArtifact;
+    }): Promise<LibTransactionDecoderContract>;
+    static deployAsync(bytecode: string, abi: ContractAbi, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: ContractAbi;
+    }): Promise<LibTransactionDecoderContract>;
+    /**
+     * @returns      The contract ABI
+     */
+    static ABI(): ContractAbi;
+    getFunctionSignature(methodName: string): string;
+    getABIDecodedTransactionData<T>(methodName: string, callData: string): T;
+    getABIDecodedReturnData<T>(methodName: string, callData: string): T;
+    getSelector(methodName: string): string;
+    /**
+     * Decodes the call data for an Exchange contract method call.
+     * @param transactionData ABI-encoded calldata for an Exchange     contract
+     *     method call.
+     * @returns The name of the function called, and the parameters it was     given.  For single-order fills and cancels, the arrays will have     just one element.
+     */
+    decodeZeroExTransactionData(transactionData: string): ContractFunctionObj<[string, Array<{
+        makerAddress: string;
+        takerAddress: string;
+        feeRecipientAddress: string;
+        senderAddress: string;
+        makerAssetAmount: BigNumber;
+        takerAssetAmount: BigNumber;
+        makerFee: BigNumber;
+        takerFee: BigNumber;
+        expirationTimeSeconds: BigNumber;
+        salt: BigNumber;
+        makerAssetData: string;
+        takerAssetData: string;
+        makerFeeAssetData: string;
+        takerFeeAssetData: string;
+    }>, BigNumber[], string[]]>;
+    constructor(address: string, supportedProvider: SupportedProvider, txDefaults?: Partial<TxData>, logDecodeDependencies?: {
+        [contractName: string]: ContractAbi;
+    }, deployedBytecode?: string | undefined);
+}
+//# sourceMappingURL=lib_transaction_decoder.d.ts.map
