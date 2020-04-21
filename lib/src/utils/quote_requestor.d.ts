2d0d8c8ad2f644b0508144e1787a0de375b3af12
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2020-04-21 02:56:15 -0400

    gitpkg

---
+++ lib/src/utils/quote_requestor.d.ts
@@ -0,0 +1,21 @@
+import { SignedOrder } from '@0x/order-utils';
+import { BigNumber } from '@0x/utils';
+import { MarketOperation, RfqtRequestOpts } from '../types';
+/**
+ * Request quotes from RFQ-T providers
+ */
+export interface RfqtIndicativeQuoteResponse {
+    makerAssetData: string;
+    makerAssetAmount: BigNumber;
+    takerAssetData: string;
+    takerAssetAmount: BigNumber;
+}
+export declare class QuoteRequestor {
+    private readonly _rfqtMakerEndpoints;
+    private readonly _schemaValidator;
+    constructor(rfqtMakerEndpoints: string[]);
+    requestRfqtFirmQuotesAsync(makerAssetData: string, takerAssetData: string, assetFillAmount: BigNumber, marketOperation: MarketOperation, options?: Partial<RfqtRequestOpts>): Promise<SignedOrder[]>;
+    requestRfqtIndicativeQuotesAsync(makerAssetData: string, takerAssetData: string, assetFillAmount: BigNumber, marketOperation: MarketOperation, options: RfqtRequestOpts): Promise<RfqtIndicativeQuoteResponse[]>;
+    private _isValidRfqtIndicativeQuoteResponse;
+}
+//# sourceMappingURL=quote_requestor.d.ts.map
