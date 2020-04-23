d2cd5459eaa84d0633b4be92d9ae792c1eabbecc
Jacob Evans <jacob@dekz.net>
2020-04-24 07:45:23 +1000

    gitpkg

---
+++ lib/src/utils/market_operation_utils/create_order.d.ts
@@ -0,0 +1,12 @@
+import { ContractAddresses } from '@0x/contract-addresses';
+import { SignedOrder } from '@0x/types';
+import { CollapsedFill, OptimizedMarketOrder, OrderDomain } from './types';
+export declare class CreateOrderUtils {
+    private readonly _contractAddress;
+    static convertNativeOrderToFullyFillableOptimizedOrders(order: SignedOrder): OptimizedMarketOrder;
+    constructor(contractAddress: ContractAddresses);
+    createSellOrdersFromPath(orderDomain: OrderDomain, inputToken: string, outputToken: string, path: CollapsedFill[], bridgeSlippage: number, liquidityProviderAddress?: string): OptimizedMarketOrder[];
+    createBuyOrdersFromPath(orderDomain: OrderDomain, inputToken: string, outputToken: string, path: CollapsedFill[], bridgeSlippage: number, liquidityProviderAddress?: string): OptimizedMarketOrder[];
+    private _getBridgeAddressFromSource;
+}
+//# sourceMappingURL=create_order.d.ts.map
