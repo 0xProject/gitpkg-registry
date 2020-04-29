1d7f8ae2e7ec8e11a1645ef5b4470999ed171a08
Jacob Evans <jacob@dekz.net>
2020-04-29 18:10:27 +1000

    gitpkg

---
+++ lib/src/utils/swap_quote_calculator.d.ts
@@ -0,0 +1,14 @@
+import { SignedOrder } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+import { CalculateSwapQuoteOpts, MarketBuySwapQuote, MarketSellSwapQuote } from '../types';
+import { MarketOperationUtils } from './market_operation_utils';
+export declare class SwapQuoteCalculator {
+    private readonly _marketOperationUtils;
+    constructor(marketOperationUtils: MarketOperationUtils);
+    calculateMarketSellSwapQuoteAsync(prunedOrders: SignedOrder[], takerAssetFillAmount: BigNumber, gasPrice: BigNumber, opts: CalculateSwapQuoteOpts): Promise<MarketSellSwapQuote>;
+    calculateMarketBuySwapQuoteAsync(prunedOrders: SignedOrder[], takerAssetFillAmount: BigNumber, gasPrice: BigNumber, opts: CalculateSwapQuoteOpts): Promise<MarketBuySwapQuote>;
+    calculateBatchMarketBuySwapQuoteAsync(batchPrunedOrders: SignedOrder[][], takerAssetFillAmounts: BigNumber[], gasPrice: BigNumber, opts: CalculateSwapQuoteOpts): Promise<Array<MarketBuySwapQuote | undefined>>;
+    private _calculateBatchBuySwapQuoteAsync;
+    private _calculateSwapQuoteAsync;
+}
+//# sourceMappingURL=swap_quote_calculator.d.ts.map
