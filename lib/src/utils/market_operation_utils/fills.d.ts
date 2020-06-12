a4308d36931c3157388e93cab4307e4d3d06c5fb
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2020-06-12 16:16:36 -0400

    gitpkg

---
+++ lib/src/utils/market_operation_utils/fills.d.ts
@@ -0,0 +1,26 @@
+import { BigNumber } from '@0x/utils';
+import { MarketOperation, SignedOrderWithFillableAmounts } from '../../types';
+import { CollapsedFill, DexSample, ERC20BridgeSource, Fill } from './types';
+/**
+ * Create fill paths from orders and dex quotes.
+ */
+export declare function createFillPaths(opts: {
+    side: MarketOperation;
+    orders?: SignedOrderWithFillableAmounts[];
+    dexQuotes?: DexSample[][];
+    targetInput?: BigNumber;
+    ethToOutputRate?: BigNumber;
+    excludedSources?: ERC20BridgeSource[];
+    feeSchedule?: {
+        [source: string]: BigNumber;
+    };
+}): Fill[][];
+export declare function getPathSize(path: Fill[], targetInput?: BigNumber): [BigNumber, BigNumber];
+export declare function getPathAdjustedSize(path: Fill[], targetInput?: BigNumber): [BigNumber, BigNumber];
+export declare function isValidPath(path: Fill[], skipDuplicateCheck?: boolean): boolean;
+export declare function clipPathToInput(path: Fill[], targetInput?: BigNumber): Fill[];
+export declare function collapsePath(path: Fill[]): CollapsedFill[];
+export declare function getFallbackSourcePaths(optimalPath: Fill[], allPaths: Fill[][]): Fill[][];
+export declare function getPathAdjustedRate(side: MarketOperation, path: Fill[], targetInput: BigNumber): BigNumber;
+export declare function getPathAdjustedSlippage(side: MarketOperation, path: Fill[], inputAmount: BigNumber, maxRate: BigNumber): number;
+//# sourceMappingURL=fills.d.ts.map
