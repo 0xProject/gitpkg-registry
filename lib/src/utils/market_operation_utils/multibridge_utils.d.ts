ddd0aa71054e7bee268e0179ac71879bc9329d0f
Daniel Pyrathon <daniel@0x.org>
2020-06-19 15:42:43 -0700

    gitpkg

---
+++ lib/src/utils/market_operation_utils/multibridge_utils.d.ts
@@ -0,0 +1,8 @@
+export declare const TOKENS: {
+    WETH: string;
+    DAI: string;
+    USDC: string;
+    MKR: string;
+};
+export declare function getMultiBridgeIntermediateToken(takerToken: string, makerToken: string): string;
+//# sourceMappingURL=multibridge_utils.d.ts.map
