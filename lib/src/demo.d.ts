ca89e0516c4cfc98e8e26899d760b0ec8d2b689b
Steve Klebanoff <steve@0xproject.com>
2020-09-04 15:45:43 -0500

    gitpkg

---
+++ lib/src/demo.d.ts
@@ -0,0 +1,7 @@
+import { BigNumber, SwapQuoteRequestOpts, FeeSchedule } from './index';
+export declare const PROTOCOL_FEE_MULTIPLIER: BigNumber;
+export declare const GAS_SCHEDULE_V0: FeeSchedule;
+export declare const GAS_SCHEDULE_V1: FeeSchedule;
+export declare const ASSET_SWAPPER_MARKET_ORDERS_OPTS: Partial<SwapQuoteRequestOpts>;
+export declare const ASSET_SWAPPER_MARKET_ORDERS_V1_OPTS: Partial<SwapQuoteRequestOpts>;
+//# sourceMappingURL=demo.d.ts.map
