013da0359532ab3a4af33688044b69315ff9db79
Steve Klebanoff <steve@0xproject.com>
2020-09-09 10:15:28 -0500

    gitpkg

---
+++ lib/src/utils/market_operation_utils/constants.d.ts
@@ -0,0 +1,43 @@
+import { BigNumber } from '@0x/utils';
+import { CurveInfo, ERC20BridgeSource, GetMarketOrdersOpts } from './types';
+/**
+ * Valid sources for market sell.
+ */
+export declare const SELL_SOURCES: ERC20BridgeSource[];
+/**
+ * Valid sources for market buy.
+ */
+export declare const BUY_SOURCES: ERC20BridgeSource[];
+export declare const DEFAULT_GET_MARKET_ORDERS_OPTS: GetMarketOrdersOpts;
+/**
+ * Sources to poll for ETH fee price estimates.
+ */
+export declare const FEE_QUOTE_SOURCES: ERC20BridgeSource[];
+/**
+ * Mainnet Curve configuration
+ */
+export declare const MAINNET_CURVE_INFOS: {
+    [name: string]: CurveInfo;
+};
+export declare const MAINNET_SWERVE_INFOS: {
+    [name: string]: CurveInfo;
+};
+export declare const MAINNET_KYBER_RESERVE_IDS: {
+    [name: string]: string;
+};
+export declare const MAINNET_KYBER_TOKEN_RESERVE_IDS: {
+    [token: string]: string;
+};
+export declare const MAINNET_SUSHI_SWAP_ROUTER = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F";
+export declare const ERC20_PROXY_ID = "0xf47261b0";
+export declare const WALLET_SIGNATURE = "0x04";
+export declare const ONE_ETHER: BigNumber;
+export declare const NEGATIVE_INF: BigNumber;
+export declare const POSITIVE_INF: BigNumber;
+export declare const ZERO_AMOUNT: BigNumber;
+export declare const MAX_UINT256: BigNumber;
+export declare const ONE_HOUR_IN_SECONDS: number;
+export declare const ONE_SECOND_MS = 1000;
+export declare const NULL_BYTES = "0x";
+export declare const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
+//# sourceMappingURL=constants.d.ts.map
---
+++ lib/src/utils/market_operation_utils/sampler_operations.d.ts
@@ -0,0 +1,53 @@
+import { SignedOrder } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+import { ERC20BridgeSamplerContract } from '../../wrappers';
+import { BalancerPoolsCache } from './balancer_utils';
+import { BancorService } from './bancor_service';
+import { BalancerFillData, BancorFillData, BatchedOperation, CurveFillData, CurveInfo, DexSample, ERC20BridgeSource, LiquidityProviderFillData, MultiBridgeFillData, MultiHopFillData, SourceQuoteOperation, SushiSwapFillData, SwerveFillData, SwerveInfo, TokenAdjacencyGraph, UniswapV2FillData } from './types';
+/**
+ * Composable operations that can be batched in a single transaction,
+ * for use with `DexOrderSampler.executeAsync()`.
+ */
+export declare class SamplerOperations {
+    protected readonly _samplerContract: ERC20BridgeSamplerContract;
+    readonly bancorService?: BancorService | undefined;
+    readonly balancerPoolsCache: BalancerPoolsCache;
+    static constant<T>(result: T): BatchedOperation<T>;
+    constructor(_samplerContract: ERC20BridgeSamplerContract, bancorService?: BancorService | undefined, balancerPoolsCache?: BalancerPoolsCache);
+    getOrderFillableTakerAmounts(orders: SignedOrder[], exchangeAddress: string): BatchedOperation<BigNumber[]>;
+    getOrderFillableMakerAmounts(orders: SignedOrder[], exchangeAddress: string): BatchedOperation<BigNumber[]>;
+    getKyberSellQuotes(reserveId: string, makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getKyberBuyQuotes(reserveId: string, makerToken: string, takerToken: string, makerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getUniswapSellQuotes(makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getUniswapBuyQuotes(makerToken: string, takerToken: string, makerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getUniswapV2SellQuotes(tokenAddressPath: string[], takerFillAmounts: BigNumber[]): SourceQuoteOperation<UniswapV2FillData>;
+    getUniswapV2BuyQuotes(tokenAddressPath: string[], makerFillAmounts: BigNumber[]): SourceQuoteOperation<UniswapV2FillData>;
+    getLiquidityProviderSellQuotes(registryAddress: string, makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): SourceQuoteOperation<LiquidityProviderFillData>;
+    getLiquidityProviderBuyQuotes(registryAddress: string, makerToken: string, takerToken: string, makerFillAmounts: BigNumber[]): SourceQuoteOperation<LiquidityProviderFillData>;
+    getMultiBridgeSellQuotes(multiBridgeAddress: string, makerToken: string, intermediateToken: string, takerToken: string, takerFillAmounts: BigNumber[]): SourceQuoteOperation<MultiBridgeFillData>;
+    getEth2DaiSellQuotes(makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getEth2DaiBuyQuotes(makerToken: string, takerToken: string, makerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getCurveSellQuotes(curve: CurveInfo, fromTokenIdx: number, toTokenIdx: number, takerFillAmounts: BigNumber[]): SourceQuoteOperation<CurveFillData>;
+    getCurveBuyQuotes(curve: CurveInfo, fromTokenIdx: number, toTokenIdx: number, makerFillAmounts: BigNumber[]): SourceQuoteOperation<CurveFillData>;
+    getSwerveSellQuotes(pool: SwerveInfo, fromTokenIdx: number, toTokenIdx: number, takerFillAmounts: BigNumber[]): SourceQuoteOperation<SwerveFillData>;
+    getSwerveBuyQuotes(pool: SwerveInfo, fromTokenIdx: number, toTokenIdx: number, makerFillAmounts: BigNumber[]): SourceQuoteOperation<SwerveFillData>;
+    getBalancerSellQuotes(poolAddress: string, makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): SourceQuoteOperation<BalancerFillData>;
+    getBalancerBuyQuotes(poolAddress: string, makerToken: string, takerToken: string, makerFillAmounts: BigNumber[]): SourceQuoteOperation<BalancerFillData>;
+    getBalancerSellQuotesOffChainAsync(makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): Promise<Array<Array<DexSample<BalancerFillData>>>>;
+    getBalancerBuyQuotesOffChainAsync(makerToken: string, takerToken: string, makerFillAmounts: BigNumber[]): Promise<Array<Array<DexSample<BalancerFillData>>>>;
+    getMStableSellQuotes(makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getMStableBuyQuotes(makerToken: string, takerToken: string, makerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getBancorSellQuotesOffChainAsync(makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): Promise<Array<DexSample<BancorFillData>>>;
+    getMooniswapSellQuotes(makerToken: string, takerToken: string, takerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getMooniswapBuyQuotes(makerToken: string, takerToken: string, makerFillAmounts: BigNumber[]): SourceQuoteOperation;
+    getTwoHopSellQuotes(sources: ERC20BridgeSource[], makerToken: string, takerToken: string, sellAmount: BigNumber, tokenAdjacencyGraph: TokenAdjacencyGraph, wethAddress: string, liquidityProviderRegistryAddress?: string): BatchedOperation<Array<DexSample<MultiHopFillData>>>;
+    getTwoHopBuyQuotes(sources: ERC20BridgeSource[], makerToken: string, takerToken: string, buyAmount: BigNumber, tokenAdjacencyGraph: TokenAdjacencyGraph, wethAddress: string, liquidityProviderRegistryAddress?: string): BatchedOperation<Array<DexSample<MultiHopFillData>>>;
+    getSushiSwapSellQuotes(tokenAddressPath: string[], takerFillAmounts: BigNumber[]): SourceQuoteOperation<SushiSwapFillData>;
+    getSushiSwapBuyQuotes(tokenAddressPath: string[], makerFillAmounts: BigNumber[]): SourceQuoteOperation<SushiSwapFillData>;
+    getMedianSellRate(sources: ERC20BridgeSource[], makerToken: string, takerToken: string, takerFillAmount: BigNumber, wethAddress: string, liquidityProviderRegistryAddress?: string, multiBridgeAddress?: string): BatchedOperation<BigNumber>;
+    getSellQuotes(sources: ERC20BridgeSource[], makerToken: string, takerToken: string, takerFillAmounts: BigNumber[], wethAddress: string, liquidityProviderRegistryAddress?: string, multiBridgeAddress?: string): BatchedOperation<DexSample[][]>;
+    getBuyQuotes(sources: ERC20BridgeSource[], makerToken: string, takerToken: string, makerFillAmounts: BigNumber[], wethAddress: string, liquidityProviderRegistryAddress?: string): BatchedOperation<DexSample[][]>;
+    private _getSellQuoteOperations;
+    private _getBuyQuoteOperations;
+}
+//# sourceMappingURL=sampler_operations.d.ts.map
