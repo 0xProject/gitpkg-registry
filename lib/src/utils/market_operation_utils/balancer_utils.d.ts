75d636bce4572340365a9b755ca7afdedb4e5740
Michael Zhu <mchl.zhu.96@gmail.com>
2020-09-29 13:16:01 -0700

    gitpkg

---
+++ lib/src/utils/market_operation_utils/balancer_utils.d.ts
@@ -0,0 +1,35 @@
+import { BigNumber } from '@0x/utils';
+export interface BalancerPool {
+    id: string;
+    balanceIn: BigNumber;
+    balanceOut: BigNumber;
+    weightIn: BigNumber;
+    weightOut: BigNumber;
+    swapFee: BigNumber;
+    spotPrice?: BigNumber;
+    slippage?: BigNumber;
+    limitAmount?: BigNumber;
+}
+interface CacheValue {
+    timestamp: number;
+    pools: BalancerPool[];
+}
+export declare class BalancerPoolsCache {
+    private readonly _cache;
+    private readonly maxPoolsFetched;
+    constructor(_cache?: {
+        [key: string]: CacheValue;
+    }, maxPoolsFetched?: number);
+    getPoolsForPairAsync(takerToken: string, makerToken: string, timeoutMs?: number): Promise<BalancerPool[]>;
+    getCachedPoolAddressesForPair(takerToken: string, makerToken: string, cacheExpiryMs?: number): string[] | undefined;
+    howToSampleBalancer(takerToken: string, makerToken: string, isAllowedSource: boolean): {
+        onChain: boolean;
+        offChain: boolean;
+    };
+    protected _getPoolsForPairAsync(takerToken: string, makerToken: string, cacheExpiryMs?: number): Promise<BalancerPool[]>;
+    protected _fetchPoolsForPairAsync(takerToken: string, makerToken: string): Promise<BalancerPool[]>;
+}
+export declare function computeBalancerSellQuote(pool: BalancerPool, takerFillAmount: BigNumber): BigNumber;
+export declare function computeBalancerBuyQuote(pool: BalancerPool, makerFillAmount: BigNumber): BigNumber;
+export {};
+//# sourceMappingURL=balancer_utils.d.ts.map
---
+++ lib/src/utils/market_operation_utils/multibridge_utils.d.ts
@@ -0,0 +1,2 @@
+export declare function getMultiBridgeIntermediateToken(takerToken: string, makerToken: string): string;
+//# sourceMappingURL=multibridge_utils.d.ts.map
