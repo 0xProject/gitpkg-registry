1974f443793bfd8f9f644cefd4a093db5be9d684
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2020-10-02 14:19:58 -0400

    gitpkg

---
+++ lib/src/artifacts.d.ts
@@ -0,0 +1,9 @@
+import { ContractArtifact } from 'ethereum-types';
+export declare const artifacts: {
+    ERC20BridgeSampler: ContractArtifact;
+    ILiquidityProvider: ContractArtifact;
+    ILiquidityProviderRegistry: ContractArtifact;
+    DummyLiquidityProviderRegistry: ContractArtifact;
+    DummyLiquidityProvider: ContractArtifact;
+};
+//# sourceMappingURL=artifacts.d.ts.map
