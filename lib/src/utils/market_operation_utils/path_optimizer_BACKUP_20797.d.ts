4d2c5fc7cc1645b8a65f20c999d66ea2ab8e0048
Jacob Evans <jacob@dekz.net>
2020-10-07 17:38:59 +1000

    gitpkg

---
+++ lib/src/utils/market_operation_utils/path_optimizer_BACKUP_20797.d.ts
@@ -0,0 +1,5 @@
+import { BigNumber } from '@0x/utils';
+import { MarketOperation } from '../../types';
+import { Fill } from './types';
+export declare function findOptimalPathAsync(side: MarketOperation, paths: Fill[][], targetInput: BigNumber, runLimit?: number): Promise<Fill[] | undefined>;
+//# sourceMappingURL=path_optimizer_BACKUP_20797.d.ts.map
