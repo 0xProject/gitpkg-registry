dbd1dc6a57a74236423475802299a261b62ca417
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2020-10-07 13:38:23 -0400

    gitpkg

---
+++ lib/src/utils/quote_requestor.d.ts
@@ -0,0 +1,25 @@
+import { RFQTFirmQuote, RFQTIndicativeQuote } from '@0x/quote-server';
+import { BigNumber } from '@0x/utils';
+import { AxiosInstance } from 'axios';
+import { LogFunction, MarketOperation, RfqtMakerAssetOfferings, RfqtRequestOpts } from '../types';
+export declare const quoteRequestorHttpClient: AxiosInstance;
+export declare class QuoteRequestor {
+    private readonly _rfqtAssetOfferings;
+    private readonly _warningLogger;
+    private readonly _infoLogger;
+    private readonly _expiryBufferMs;
+    private readonly _schemaValidator;
+    private readonly _orderSignatureToMakerUri;
+    constructor(_rfqtAssetOfferings: RfqtMakerAssetOfferings, _warningLogger?: LogFunction, _infoLogger?: LogFunction, _expiryBufferMs?: number);
+    requestRfqtFirmQuotesAsync(makerAssetData: string, takerAssetData: string, assetFillAmount: BigNumber, marketOperation: MarketOperation, options: RfqtRequestOpts): Promise<RFQTFirmQuote[]>;
+    requestRfqtIndicativeQuotesAsync(makerAssetData: string, takerAssetData: string, assetFillAmount: BigNumber, marketOperation: MarketOperation, options: RfqtRequestOpts): Promise<RFQTIndicativeQuote[]>;
+    /**
+     * Given an order signature, returns the makerUri that the order originated from
+     */
+    getMakerUriForOrderSignature(orderSignature: string): string | undefined;
+    private _isValidRfqtIndicativeQuoteResponse;
+    private _makerSupportsPair;
+    private _isExpirationTooSoon;
+    private _getQuotesAsync;
+}
+//# sourceMappingURL=quote_requestor.d.ts.map
