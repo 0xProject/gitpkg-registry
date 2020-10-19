c7bd02f8361272aa235daf7f83a7633ce851aac1
Romain Butteaud <romain.butteaud@gmail.com>
2020-10-19 16:23:00 -0700

    gitpkg

---
+++ lib/test/generated-wrappers/sushi_swap_sampler.d.ts
@@ -0,0 +1,59 @@
+import { ContractFunctionObj, BaseContract } from '@0x/base-contract';
+import { ContractAbi, ContractArtifact, TxData, SupportedProvider } from 'ethereum-types';
+import { BigNumber } from '@0x/utils';
+import { SimpleContractArtifact } from '@0x/types';
+import { Web3Wrapper } from '@0x/web3-wrapper';
+export declare class SushiSwapSamplerContract extends BaseContract {
+    /**
+     * @ignore
+     */
+    static deployedBytecode: string | undefined;
+    static contractName: string;
+    private readonly _methodABIIndex;
+    static deployFrom0xArtifactAsync(artifact: ContractArtifact | SimpleContractArtifact, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }): Promise<SushiSwapSamplerContract>;
+    static deployWithLibrariesFrom0xArtifactAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }): Promise<SushiSwapSamplerContract>;
+    static deployAsync(bytecode: string, abi: ContractAbi, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: ContractAbi;
+    }): Promise<SushiSwapSamplerContract>;
+    /**
+     * @returns      The contract ABI
+     */
+    static ABI(): ContractAbi;
+    protected static _deployLibrariesAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, web3Wrapper: Web3Wrapper, txDefaults: Partial<TxData>, libraryAddresses?: {
+        [libraryName: string]: string;
+    }): Promise<{
+        [libraryName: string]: string;
+    }>;
+    getFunctionSignature(methodName: string): string;
+    getABIDecodedTransactionData<T>(methodName: string, callData: string): T;
+    getABIDecodedReturnData<T>(methodName: string, callData: string): T;
+    getSelector(methodName: string): string;
+    /**
+     * Sample buy quotes from SushiSwap
+      * @param router Router to look up tokens and amounts
+      * @param path Token route. Should be takerToken -> makerToken.
+      * @param makerTokenAmounts Maker token buy amount for each sample.
+    * @returns takerTokenAmounts Taker amounts sold at each maker token         amount.
+     */
+    sampleBuysFromSushiSwap(router: string, path: string[], makerTokenAmounts: BigNumber[]): ContractFunctionObj<BigNumber[]>;
+    /**
+     * Sample sell quotes from SushiSwap.
+      * @param router Router to look up tokens and amounts
+      * @param path Token route. Should be takerToken -> makerToken
+      * @param takerTokenAmounts Taker token sell amount for each sample.
+    * @returns makerTokenAmounts Maker amounts bought at each taker token         amount.
+     */
+    sampleSellsFromSushiSwap(router: string, path: string[], takerTokenAmounts: BigNumber[]): ContractFunctionObj<BigNumber[]>;
+    constructor(address: string, supportedProvider: SupportedProvider, txDefaults?: Partial<TxData>, logDecodeDependencies?: {
+        [contractName: string]: ContractAbi;
+    }, deployedBytecode?: string | undefined);
+}
+//# sourceMappingURL=sushi_swap_sampler.d.ts.map
