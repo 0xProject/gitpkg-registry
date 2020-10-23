de9854b002c4780635d7d5d0cb896c06cb8cad17
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2020-10-23 14:15:38 -0400

    gitpkg

---
+++ lib/src/utils/market_operation_utils/curve_utils.d.ts
@@ -0,0 +1,5 @@
+import { CurveInfo, SnowSwapInfo, SwerveInfo } from './types';
+export declare function getCurveInfosForPair(takerToken: string, makerToken: string): CurveInfo[];
+export declare function getSwerveInfosForPair(takerToken: string, makerToken: string): SwerveInfo[];
+export declare function getSnowSwapInfosForPair(takerToken: string, makerToken: string): SnowSwapInfo[];
+//# sourceMappingURL=curve_utils.d.ts.map
