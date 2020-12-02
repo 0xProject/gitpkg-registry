ee70cc8545717d5f07716b296052ddc935812c7e
Alex Kroeger <alexrkroeger@gmail.com>
2020-12-02 14:02:18 -0800

    gitpkg

---
+++ lib/src/utils/rfqt_mocker.d.ts
@@ -0,0 +1,16 @@
+import { AxiosInstance } from 'axios';
+import { MockedRfqtFirmQuoteResponse } from '../types';
+/**
+ * A helper utility for testing which mocks out
+ * requests to RFQ-t providers
+ */
+export declare const rfqtMocker: {
+    /**
+     * Stubs out responses from RFQ-T providers by mocking out
+     * HTTP calls via axios. Always restores the mock adapter
+     * after executing the `performFn`.
+     */
+    withMockedRfqtFirmQuotes: (mockedResponses: MockedRfqtFirmQuoteResponse[], performFn: () => Promise<void>, axiosClient?: AxiosInstance) => Promise<void>;
+    withMockedRfqtIndicativeQuotes: (mockedResponses: MockedRfqtFirmQuoteResponse[], performFn: () => Promise<void>, axiosClient?: AxiosInstance) => Promise<void>;
+};
+//# sourceMappingURL=rfqt_mocker.d.ts.map
---
+++ lib/test/artifacts.d.ts
@@ -0,0 +1,37 @@
+import { ContractArtifact } from 'ethereum-types';
+export declare const artifacts: {
+    ApproximateBuys: ContractArtifact;
+    BalanceChecker: ContractArtifact;
+    BalancerSampler: ContractArtifact;
+    CurveSampler: ContractArtifact;
+    DODOSampler: ContractArtifact;
+    DeploymentConstants: ContractArtifact;
+    ERC20BridgeSampler: ContractArtifact;
+    Eth2DaiSampler: ContractArtifact;
+    KyberSampler: ContractArtifact;
+    LiquidityProviderSampler: ContractArtifact;
+    MStableSampler: ContractArtifact;
+    MooniswapSampler: ContractArtifact;
+    MultiBridgeSampler: ContractArtifact;
+    NativeOrderSampler: ContractArtifact;
+    SamplerUtils: ContractArtifact;
+    ShellSampler: ContractArtifact;
+    SushiSwapSampler: ContractArtifact;
+    TwoHopSampler: ContractArtifact;
+    UniswapSampler: ContractArtifact;
+    UniswapV2Sampler: ContractArtifact;
+    IBalancer: ContractArtifact;
+    ICurve: ContractArtifact;
+    IEth2Dai: ContractArtifact;
+    IKyberNetwork: ContractArtifact;
+    IMStable: ContractArtifact;
+    IMooniswap: ContractArtifact;
+    IMultiBridge: ContractArtifact;
+    IShell: ContractArtifact;
+    IUniswapExchangeQuotes: ContractArtifact;
+    IUniswapV2Router01: ContractArtifact;
+    DummyLiquidityProvider: ContractArtifact;
+    TestERC20BridgeSampler: ContractArtifact;
+    TestNativeOrderSampler: ContractArtifact;
+};
+//# sourceMappingURL=artifacts.d.ts.map
