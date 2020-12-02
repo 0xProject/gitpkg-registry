c0a3a45b317696e9e2881e42da71cbc0f8273079
Alex Kroeger <alexrkroeger@gmail.com>
2020-12-01 17:45:14 -0800

    gitpkg

---
+++ lib/src/utils/market_operation_utils/sampler_contract_operation.d.ts
@@ -0,0 +1,23 @@
+import { ContractFunctionObj } from '@0x/base-contract';
+import { BigNumber } from '@0x/utils';
+import { ERC20BridgeSamplerContract } from '../../wrappers';
+import { ERC20BridgeSource, FillData, SourceInfo, SourceQuoteOperation } from './types';
+export declare type Parameters<T> = T extends (...args: infer TArgs) => any ? TArgs : never;
+export interface SamplerContractCall<TFunc extends (...args: any[]) => ContractFunctionObj<any>, TFillData extends FillData = FillData> {
+    contract: ERC20BridgeSamplerContract;
+    function: TFunc;
+    params: Parameters<TFunc>;
+    callback?: (callResults: string, fillData: TFillData) => BigNumber[];
+}
+export declare class SamplerContractOperation<TFunc extends (...args: any[]) => ContractFunctionObj<any>, TFillData extends FillData = FillData> implements SourceQuoteOperation<TFillData> {
+    readonly source: ERC20BridgeSource;
+    fillData: TFillData;
+    private readonly _samplerContract;
+    private readonly _samplerFunction;
+    private readonly _params;
+    private readonly _callback?;
+    constructor(opts: SourceInfo<TFillData> & SamplerContractCall<TFunc, TFillData>);
+    encodeCall(): string;
+    handleCallResults(callResults: string): BigNumber[];
+}
+//# sourceMappingURL=sampler_contract_operation.d.ts.map
---
+++ lib/test/utils/mock_sampler_contract.d.ts
@@ -0,0 +1,46 @@
+import { ContractTxFunctionObj } from '@0x/base-contract';
+import { Order } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+import { ERC20BridgeSamplerContract } from '../../src/wrappers';
+export declare type GetOrderFillableAssetAmountResult = BigNumber[];
+export declare type GetOrderFillableAssetAmountHandler = (orders: Order[], signatures: string[], devUtilsAddress: string) => GetOrderFillableAssetAmountResult;
+export declare type SampleResults = BigNumber[];
+export declare type SampleSellsHandler = (takerToken: string, makerToken: string, takerTokenAmounts: BigNumber[]) => SampleResults;
+export declare type SampleBuysHandler = (takerToken: string, makerToken: string, makerTokenAmounts: BigNumber[]) => SampleResults;
+export declare type SampleSellsKyberHandler = (reserveId: string, takerToken: string, makerToken: string, takerTokenAmounts: BigNumber[]) => [string, SampleResults];
+export declare type SampleBuysKyberHandler = (reserveId: string, takerToken: string, makerToken: string, makerTokenAmounts: BigNumber[]) => [string, SampleResults];
+export declare type SampleBuysMultihopHandler = (path: string[], takerTokenAmounts: BigNumber[]) => SampleResults;
+export declare type SampleSellsLPHandler = (providerAddress: string, takerToken: string, makerToken: string, takerTokenAmounts: BigNumber[]) => SampleResults;
+export declare type SampleSellsMultihopHandler = (path: string[], takerTokenAmounts: BigNumber[]) => SampleResults;
+interface Handlers {
+    getOrderFillableMakerAssetAmounts: GetOrderFillableAssetAmountHandler;
+    getOrderFillableTakerAssetAmounts: GetOrderFillableAssetAmountHandler;
+    sampleSellsFromKyberNetwork: SampleSellsKyberHandler;
+    sampleSellsFromLiquidityProvider: SampleSellsLPHandler;
+    sampleSellsFromEth2Dai: SampleSellsHandler;
+    sampleSellsFromUniswap: SampleSellsHandler;
+    sampleSellsFromUniswapV2: SampleSellsMultihopHandler;
+    sampleBuysFromEth2Dai: SampleBuysHandler;
+    sampleBuysFromUniswap: SampleBuysHandler;
+    sampleBuysFromUniswapV2: SampleBuysMultihopHandler;
+    sampleBuysFromLiquidityProvider: SampleSellsLPHandler;
+}
+export declare class MockSamplerContract extends ERC20BridgeSamplerContract {
+    private readonly _handlers;
+    constructor(handlers?: Partial<Handlers>);
+    batchCall(callDatas: string[]): ContractTxFunctionObj<string[]>;
+    getOrderFillableMakerAssetAmounts(orders: Order[], signatures: string[]): ContractTxFunctionObj<GetOrderFillableAssetAmountResult>;
+    getOrderFillableTakerAssetAmounts(orders: Order[], signatures: string[]): ContractTxFunctionObj<GetOrderFillableAssetAmountResult>;
+    sampleSellsFromKyberNetwork(reserveId: string, takerToken: string, makerToken: string, takerAssetAmounts: BigNumber[]): ContractTxFunctionObj<[string, BigNumber[]]>;
+    sampleSellsFromEth2Dai(takerToken: string, makerToken: string, takerAssetAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    sampleSellsFromUniswap(takerToken: string, makerToken: string, takerAssetAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    sampleSellsFromUniswapV2(path: string[], takerAssetAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    sampleSellsFromLiquidityProvider(providerAddress: string, takerToken: string, makerToken: string, takerAssetAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    sampleBuysFromEth2Dai(takerToken: string, makerToken: string, makerAssetAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    sampleBuysFromUniswap(takerToken: string, makerToken: string, makerAssetAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    sampleBuysFromUniswapV2(path: string[], makerAssetAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    private _callEncodedFunction;
+    private _wrapCall;
+}
+export {};
+//# sourceMappingURL=mock_sampler_contract.d.ts.map
