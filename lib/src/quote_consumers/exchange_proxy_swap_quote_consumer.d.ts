410303dcb59ff30d515c220090e17cc7d74ebfd7
Michael Zhu <mchl.zhu.96@gmail.com>
2020-12-02 11:32:52 -0800

    gitpkg

---
+++ lib/src/quote_consumers/exchange_proxy_swap_quote_consumer.d.ts
@@ -0,0 +1,19 @@
+import { ContractAddresses } from '@0x/contract-addresses';
+import { SupportedProvider, ZeroExProvider } from '@0x/web3-wrapper';
+import { CalldataInfo, MarketBuySwapQuote, MarketSellSwapQuote, SwapQuote, SwapQuoteConsumerBase, SwapQuoteConsumerOpts, SwapQuoteExecutionOpts, SwapQuoteGetOutputOpts } from '../types';
+export declare class ExchangeProxySwapQuoteConsumer implements SwapQuoteConsumerBase {
+    readonly contractAddresses: ContractAddresses;
+    readonly provider: ZeroExProvider;
+    readonly chainId: number;
+    readonly transformerNonces: {
+        wethTransformer: number;
+        payTakerTransformer: number;
+        fillQuoteTransformer: number;
+        affiliateFeeTransformer: number;
+    };
+    private readonly _exchangeProxy;
+    constructor(supportedProvider: SupportedProvider, contractAddresses: ContractAddresses, options?: Partial<SwapQuoteConsumerOpts>);
+    getCalldataOrThrowAsync(quote: MarketBuySwapQuote | MarketSellSwapQuote, opts?: Partial<SwapQuoteGetOutputOpts>): Promise<CalldataInfo>;
+    executeSwapQuoteOrThrowAsync(_quote: SwapQuote, _opts: Partial<SwapQuoteExecutionOpts>): Promise<string>;
+}
+//# sourceMappingURL=exchange_proxy_swap_quote_consumer.d.ts.map
