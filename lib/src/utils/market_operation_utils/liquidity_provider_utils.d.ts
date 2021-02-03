ee4513c3303a12ac2dead3021759babca7ae7605
xianny <xianny@gmail.com>
2021-02-03 11:29:16 -0800

    gitpkg

---
+++ lib/src/utils/market_operation_utils/liquidity_provider_utils.d.ts
@@ -0,0 +1,3 @@
+import { LiquidityProviderRegistry } from './types';
+export declare function getLiquidityProvidersForPair(registry: LiquidityProviderRegistry, takerToken: string, makerToken: string): string[];
+//# sourceMappingURL=liquidity_provider_utils.d.ts.map
