8e29169f07d730572652aaaad849b671016ce0b9
xianny <xianny@gmail.com>
2021-02-03 11:03:40 -0800

    gitpkg

---
+++ lib/src/artifacts.d.ts
@@ -0,0 +1,6 @@
+import { ContractArtifact } from 'ethereum-types';
+export declare const artifacts: {
+    ERC20BridgeSampler: ContractArtifact;
+    BalanceChecker: ContractArtifact;
+};
+//# sourceMappingURL=artifacts.d.ts.map
