d9cd7eaa03f39143d2e14115d88bff5266994d03
xianny <xianny@gmail.com>
2021-02-03 19:44:15 -0800

    gitpkg

---
+++ lib/src/utils/swap_quote_consumer_utils.d.ts
@@ -0,0 +1,13 @@
+import { ContractAddresses } from '@0x/contract-addresses';
+import { BigNumber } from '@0x/utils';
+import { SupportedProvider } from '@0x/web3-wrapper';
+import { Provider } from 'ethereum-types';
+import { ExtensionContractType, GetExtensionContractTypeOpts, SwapQuote, SwapQuoteExecutionOpts } from '../types';
+export declare const swapQuoteConsumerUtils: {
+    getTakerAddressOrThrowAsync(provider: SupportedProvider, opts: Partial<SwapQuoteExecutionOpts>): Promise<string>;
+    getTakerAddressAsync(provider: SupportedProvider, opts: Partial<SwapQuoteExecutionOpts>): Promise<string | undefined>;
+    getEthAndWethBalanceAsync(provider: SupportedProvider, contractAddresses: ContractAddresses, takerAddress: string): Promise<[BigNumber, BigNumber]>;
+    isValidForwarderSwapQuote(swapQuote: SwapQuote, wethToken: string): boolean;
+    getExtensionContractTypeForSwapQuoteAsync(quote: SwapQuote, contractAddresses: ContractAddresses, provider: Provider, opts: Partial<GetExtensionContractTypeOpts>): Promise<ExtensionContractType>;
+};
+//# sourceMappingURL=swap_quote_consumer_utils.d.ts.map
