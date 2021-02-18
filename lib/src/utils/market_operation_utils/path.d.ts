e9abc06ff162a71bbb814e83b0a6cd3a57247d7c
Jacob Evans <jacob@dekz.net>
2021-02-18 14:25:30 +1000

    gitpkg

---
+++ lib/src/utils/market_operation_utils/path.d.ts
@@ -0,0 +1,47 @@
+import { BigNumber } from '@0x/utils';
+import { MarketOperation } from '../../types';
+import { CreateOrderFromPathOpts } from './orders';
+import { CollapsedFill, ExchangeProxyOverhead, Fill, OptimizedMarketOrder } from './types';
+export interface PathSize {
+    input: BigNumber;
+    output: BigNumber;
+}
+export interface PathPenaltyOpts {
+    ethToOutputRate: BigNumber;
+    ethToInputRate: BigNumber;
+    exchangeProxyOverhead: ExchangeProxyOverhead;
+}
+export declare const DEFAULT_PATH_PENALTY_OPTS: PathPenaltyOpts;
+export declare class Path {
+    protected readonly side: MarketOperation;
+    fills: ReadonlyArray<Fill>;
+    protected readonly targetInput: BigNumber;
+    readonly pathPenaltyOpts: PathPenaltyOpts;
+    collapsedFills?: ReadonlyArray<CollapsedFill>;
+    orders?: OptimizedMarketOrder[];
+    sourceFlags: number;
+    protected _size: PathSize;
+    protected _adjustedSize: PathSize;
+    static create(side: MarketOperation, fills: ReadonlyArray<Fill>, targetInput?: BigNumber, pathPenaltyOpts?: PathPenaltyOpts): Path;
+    static clone(base: Path): Path;
+    protected constructor(side: MarketOperation, fills: ReadonlyArray<Fill>, targetInput: BigNumber, pathPenaltyOpts: PathPenaltyOpts);
+    append(fill: Fill): this;
+    addFallback(fallback: Path): this;
+    collapse(opts: CreateOrderFromPathOpts): CollapsedPath;
+    size(): PathSize;
+    adjustedSize(): PathSize;
+    adjustedCompleteRate(): BigNumber;
+    adjustedRate(): BigNumber;
+    adjustedSlippage(maxRate: BigNumber): number;
+    isBetterThan(other: Path): boolean;
+    isComplete(): boolean;
+    isValid(skipDuplicateCheck?: boolean): boolean;
+    isValidNextFill(fill: Fill): boolean;
+    private _collapseFills;
+    private _addFillSize;
+}
+export interface CollapsedPath extends Path {
+    readonly collapsedFills: ReadonlyArray<CollapsedFill>;
+    readonly orders: OptimizedMarketOrder[];
+}
+//# sourceMappingURL=path.d.ts.map
