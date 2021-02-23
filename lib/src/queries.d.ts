1a9eda8a6d35eef900ed2c2b8efbd678e7d0fb0f
Kim Persson <kimpersson88@gmail.com>
2021-02-23 16:39:52 +0100

    gitpkg

---
+++ lib/src/queries.d.ts
@@ -0,0 +1,9 @@
+export declare const statsQuery: import("graphql/language/ast").DocumentNode;
+export declare const addOrdersMutation: import("graphql/language/ast").DocumentNode;
+export declare const addOrdersMutationV4: import("graphql/language/ast").DocumentNode;
+export declare const orderQuery: import("graphql/language/ast").DocumentNode;
+export declare const orderQueryV4: import("graphql/language/ast").DocumentNode;
+export declare const ordersQuery: import("graphql/language/ast").DocumentNode;
+export declare const ordersQueryV4: import("graphql/language/ast").DocumentNode;
+export declare const orderEventsSubscription: import("graphql/language/ast").DocumentNode;
+//# sourceMappingURL=queries.d.ts.map
