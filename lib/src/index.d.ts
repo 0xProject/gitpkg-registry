3d5cbe57f47050875b5102cf9a029af6f632aaf3
xianny <xianny@gmail.com>
2021-03-05 13:00:13 -0800

    gitpkg

---
+++ lib/src/index.d.ts
@@ -0,0 +1,7 @@
+export { HealthcheckService, createHealthcheckRouter } from './healthcheck';
+export { MetricsService, createMetricsRouter } from './metrics';
+export { createDefaultServer } from './default_server';
+export { createRequestLoggerMiddleware } from './middleware/request_logger';
+export { cacheControl } from './middleware/cache_control';
+export { HttpServiceConfig, GenericLogger } from './types';
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/src/middleware/request_logger.d.ts
@@ -0,0 +1,7 @@
+import * as core from 'express-serve-static-core';
+import { GenericLogger } from '../types';
+/**
+ * log middleware
+ */
+export declare function createRequestLoggerMiddleware(logger: GenericLogger): core.RequestHandler;
+//# sourceMappingURL=request_logger.d.ts.map
