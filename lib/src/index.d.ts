7ced426daf41109330e48a5b325d2cd29aa4864c
xianny <xianny@gmail.com>
2021-03-30 14:59:34 -0700

    gitpkg

---
+++ lib/src/index.d.ts
@@ -0,0 +1,10 @@
+export { createDefaultServer } from './default_server';
+export { HealthcheckService, createHealthcheckRouter } from './healthcheck';
+export { MetricsService, createMetricsRouter } from './metrics';
+export { createRequestLoggerMiddleware } from './middleware/request_logger';
+export { cacheControl } from './middleware/cache_control';
+export { ErrorUtils, isAlertingError, isAPIError, isBadRequestError, isInternalServerError, isRevertAPIError, isRevertError, isValidationError, } from './middleware/error_handler';
+export { AlertError, APIBaseError, BadRequestError, ErrorBodyWithHTTPStatusCode, ErrorBody, GeneralErrorCodes, generalErrorCodeToReason, InternalServerError, InvalidAPIKeyError, MalformedJSONError, NotFoundError, NotImplementedError, RevertAPIError, RevertReasonErrorBody, TooManyRequestsError, ValidationError, ValidationErrorCodes, ValidationErrorItem, } from './errors';
+export { APISchemaValidator } from './schema_utils';
+export { HttpServiceConfig, GenericLogger } from './types';
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/src/middleware/cache_control.d.ts
@@ -0,0 +1,6 @@
+import { Handler } from 'express';
+/**
+ *  Sets the Cache related headers in a response
+ */
+export declare function cacheControl(DEFAULT_CACHE_AGE_SECONDS: number): Handler;
+//# sourceMappingURL=cache_control.d.ts.map
