905b9126544982f8a641c5c4e9c673de6d37ac75
Jacob Evans <jacob@dekz.net>
2021-04-26 16:40:15 +1000

    gitpkg

---
+++ lib/src/quote_consumers/exchange_proxy_swap_quote_consumer.d.ts
@@ -0,0 +1,23 @@
+import { ChainId, ContractAddresses } from '@0x/contract-addresses';
+import { SupportedProvider, ZeroExProvider } from '@0x/web3-wrapper';
+import { CalldataInfo, MarketBuySwapQuote, MarketSellSwapQuote, SwapQuote, SwapQuoteConsumerBase, SwapQuoteConsumerOpts, SwapQuoteExecutionOpts, SwapQuoteGetOutputOpts } from '../types';
+export declare class ExchangeProxySwapQuoteConsumer implements SwapQuoteConsumerBase {
+    readonly contractAddresses: ContractAddresses;
+    readonly provider: ZeroExProvider;
+    readonly chainId: ChainId;
+    readonly transformerNonces: {
+        wethTransformer: number;
+        payTakerTransformer: number;
+        fillQuoteTransformer: number;
+        affiliateFeeTransformer: number;
+        positiveSlippageFeeTransformer: number;
+    };
+    private readonly _exchangeProxy;
+    private readonly _multiplex;
+    constructor(supportedProvider: SupportedProvider, contractAddresses: ContractAddresses, options?: Partial<SwapQuoteConsumerOpts>);
+    getCalldataOrThrowAsync(quote: MarketBuySwapQuote | MarketSellSwapQuote, opts?: Partial<SwapQuoteGetOutputOpts>): Promise<CalldataInfo>;
+    executeSwapQuoteOrThrowAsync(_quote: SwapQuote, _opts: Partial<SwapQuoteExecutionOpts>): Promise<string>;
+    private _encodeMultiplexBatchFillCalldata;
+    private _encodeMultiplexMultiHopFillCalldata;
+}
+//# sourceMappingURL=exchange_proxy_swap_quote_consumer.d.ts.map
---
+++ lib/test/generated-wrappers/i_smoothy.d.ts
@@ -0,0 +1,46 @@
+import { ContractTxFunctionObj, BaseContract } from '@0x/base-contract';
+import { ContractAbi, ContractArtifact, TxData, SupportedProvider } from 'ethereum-types';
+import { AbiEncoder, BigNumber } from '@0x/utils';
+import { SimpleContractArtifact } from '@0x/types';
+import { Web3Wrapper } from '@0x/web3-wrapper';
+export declare class ISmoothyContract extends BaseContract {
+    /**
+     * @ignore
+     */
+    static deployedBytecode: string | undefined;
+    static contractName: string;
+    private readonly _methodABIIndex;
+    static deployFrom0xArtifactAsync(artifact: ContractArtifact | SimpleContractArtifact, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }): Promise<ISmoothyContract>;
+    static deployWithLibrariesFrom0xArtifactAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }): Promise<ISmoothyContract>;
+    static deployAsync(bytecode: string, abi: ContractAbi, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: ContractAbi;
+    }): Promise<ISmoothyContract>;
+    /**
+     * @returns      The contract ABI
+     */
+    static ABI(): ContractAbi;
+    protected static _deployLibrariesAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, web3Wrapper: Web3Wrapper, txDefaults: Partial<TxData>, libraryAddresses?: {
+        [libraryName: string]: string;
+    }): Promise<{
+        [libraryName: string]: string;
+    }>;
+    getFunctionSignature(methodName: string): string;
+    getABIDecodedTransactionData<T>(methodName: string, callData: string): T;
+    getABIDecodedReturnData<T>(methodName: string, callData: string): T;
+    getSelector(methodName: string): string;
+    _yBalances(tid: BigNumber): ContractTxFunctionObj<BigNumber>;
+    getBalance(tid: BigNumber): ContractTxFunctionObj<BigNumber>;
+    getTokenStats(tid: BigNumber): ContractTxFunctionObj<[BigNumber, BigNumber, BigNumber, BigNumber]>;
+    constructor(address: string, supportedProvider: SupportedProvider, txDefaults?: Partial<TxData>, logDecodeDependencies?: {
+        [contractName: string]: ContractAbi;
+    }, deployedBytecode?: string | undefined, encodingRules?: AbiEncoder.EncodingRules);
+}
+//# sourceMappingURL=i_smoothy.d.ts.map
