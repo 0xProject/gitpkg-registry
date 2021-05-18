576876808681f67d5347f2490f4b3db100a7bd5f
Kim Persson <kimpersson88@gmail.com>
2021-05-18 13:51:43 +0200

    gitpkg

---
+++ lib/token_metadata.d.ts
@@ -0,0 +1,18 @@
+import { ChainId } from '@0x/contract-addresses';
+export interface TokenMetadataAndChainAddresses {
+    symbol: string;
+    decimals: number;
+    name: string;
+    tokenAddresses: {
+        [ChainId.Mainnet]: string;
+        [ChainId.Kovan]: string;
+        [ChainId.Ganache]: string;
+        [ChainId.Rinkeby]: string;
+        [ChainId.Ropsten]: string;
+        [ChainId.BSC]: string;
+        [ChainId.Polygon]: string;
+        [ChainId.PolygonMumbai]: string;
+    };
+}
+export declare const TokenMetadatasForChains: TokenMetadataAndChainAddresses[];
+//# sourceMappingURL=token_metadata.d.ts.map
