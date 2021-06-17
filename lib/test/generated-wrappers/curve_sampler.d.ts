cb9694ba7bda71ecaf608059d6720a483528867c
Jacob Evans <jacob@dekz.net>
2021-06-18 08:34:04 +1000

    gitpkg

---
+++ lib/test/generated-wrappers/curve_sampler.d.ts
@@ -0,0 +1,75 @@
+import { EncoderOverrides, ContractTxFunctionObj, BaseContract } from '@0x/base-contract';
+import { ContractAbi, ContractArtifact, TxData, SupportedProvider } from 'ethereum-types';
+import { BigNumber } from '@0x/utils';
+import { SimpleContractArtifact } from '@0x/types';
+import { Web3Wrapper } from '@0x/web3-wrapper';
+export declare class CurveSamplerContract extends BaseContract {
+    /**
+     * @ignore
+     */
+    static deployedBytecode: string | undefined;
+    static contractName: string;
+    private readonly _methodABIIndex;
+    static deployFrom0xArtifactAsync(artifact: ContractArtifact | SimpleContractArtifact, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }, weth: string): Promise<CurveSamplerContract>;
+    static deployWithLibrariesFrom0xArtifactAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }, weth: string): Promise<CurveSamplerContract>;
+    static deployAsync(bytecode: string, abi: ContractAbi, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: ContractAbi;
+    }, weth: string): Promise<CurveSamplerContract>;
+    /**
+     * @returns      The contract ABI
+     */
+    static ABI(): ContractAbi;
+    protected static _deployLibrariesAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, web3Wrapper: Web3Wrapper, txDefaults: Partial<TxData>, libraryAddresses?: {
+        [libraryName: string]: string;
+    }): Promise<{
+        [libraryName: string]: string;
+    }>;
+    getFunctionSignature(methodName: string): string;
+    getABIDecodedTransactionData<T>(methodName: string, callData: string): T;
+    getABIDecodedReturnData<T>(methodName: string, callData: string): T;
+    getSelector(methodName: string): string;
+    _callRevert(selector: string, sellToken: string, buyToken: string, bridgeData: string, amountIn: BigNumber): ContractTxFunctionObj<void>;
+    /**
+     * Mints the sell token, then performs the swap, then reverts with the amount out. The SwapRevertSamplerQuoteOpts has been unrolled here as our ABI encoder cannot support encoding the function
+     */
+    _mintCallRevert(selector: string, sellToken: string, buyToken: string, bridgeData: string, amountsIn: BigNumber[]): ContractTxFunctionObj<void>;
+    /**
+     * Sample buy quotes from Curve.
+      * @param curveInfo Curve information specific to this token pair.
+      * @param takerToken The taker token to sell.
+      * @param makerToken The maker token to buy.
+      * @param makerTokenAmounts Maker token buy amount for each sample.
+     */
+    sampleBuysFromCurve(curveInfo: {
+        curveAddress: string;
+        exchangeFunctionSelector: string;
+        fromCoinIdx: BigNumber;
+        toCoinIdx: BigNumber;
+    }, takerToken: string, makerToken: string, makerTokenAmounts: BigNumber[]): ContractTxFunctionObj<[BigNumber[], BigNumber[]]>;
+    /**
+     * Sample sell quotes from Curve.
+      * @param curveInfo Curve information specific to this token pair.
+      * @param takerToken The taker token to sell.
+      * @param makerToken The maker token to buy.
+      * @param takerTokenAmounts Taker token sell amount for each sample.
+     */
+    sampleSellsFromCurve(curveInfo: {
+        curveAddress: string;
+        exchangeFunctionSelector: string;
+        fromCoinIdx: BigNumber;
+        toCoinIdx: BigNumber;
+    }, takerToken: string, makerToken: string, takerTokenAmounts: BigNumber[]): ContractTxFunctionObj<[BigNumber[], BigNumber[]]>;
+    sampleSwapFromCurve(sellToken: string, buyToken: string, bridgeData: string, takerTokenAmount: BigNumber): ContractTxFunctionObj<BigNumber>;
+    constructor(address: string, supportedProvider: SupportedProvider, txDefaults?: Partial<TxData>, logDecodeDependencies?: {
+        [contractName: string]: ContractAbi;
+    }, deployedBytecode?: string | undefined, encoderOverrides?: Partial<EncoderOverrides>);
+}
+//# sourceMappingURL=curve_sampler.d.ts.map
---
+++ lib/test/generated-wrappers/uniswap_sampler.d.ts
@@ -0,0 +1,64 @@
+import { EncoderOverrides, ContractTxFunctionObj, BaseContract } from '@0x/base-contract';
+import { ContractAbi, ContractArtifact, TxData, SupportedProvider } from 'ethereum-types';
+import { BigNumber } from '@0x/utils';
+import { SimpleContractArtifact } from '@0x/types';
+import { Web3Wrapper } from '@0x/web3-wrapper';
+export declare class UniswapSamplerContract extends BaseContract {
+    /**
+     * @ignore
+     */
+    static deployedBytecode: string | undefined;
+    static contractName: string;
+    private readonly _methodABIIndex;
+    static deployFrom0xArtifactAsync(artifact: ContractArtifact | SimpleContractArtifact, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }, weth: string): Promise<UniswapSamplerContract>;
+    static deployWithLibrariesFrom0xArtifactAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }, weth: string): Promise<UniswapSamplerContract>;
+    static deployAsync(bytecode: string, abi: ContractAbi, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: ContractAbi;
+    }, weth: string): Promise<UniswapSamplerContract>;
+    /**
+     * @returns      The contract ABI
+     */
+    static ABI(): ContractAbi;
+    protected static _deployLibrariesAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, web3Wrapper: Web3Wrapper, txDefaults: Partial<TxData>, libraryAddresses?: {
+        [libraryName: string]: string;
+    }): Promise<{
+        [libraryName: string]: string;
+    }>;
+    getFunctionSignature(methodName: string): string;
+    getABIDecodedTransactionData<T>(methodName: string, callData: string): T;
+    getABIDecodedReturnData<T>(methodName: string, callData: string): T;
+    getSelector(methodName: string): string;
+    _callRevert(selector: string, sellToken: string, buyToken: string, bridgeData: string, amountIn: BigNumber): ContractTxFunctionObj<void>;
+    /**
+     * Mints the sell token, then performs the swap, then reverts with the amount out. The SwapRevertSamplerQuoteOpts has been unrolled here as our ABI encoder cannot support encoding the function
+     */
+    _mintCallRevert(selector: string, sellToken: string, buyToken: string, bridgeData: string, amountsIn: BigNumber[]): ContractTxFunctionObj<void>;
+    /**
+     * Sample buy quotes from Uniswap.
+      * @param takerToken Address of the taker token (what to sell).
+      * @param makerToken Address of the maker token (what to buy).
+      * @param makerTokenAmounts Maker token sell amount for each sample.
+     */
+    sampleBuysFromUniswap(router: string, takerToken: string, makerToken: string, makerTokenAmounts: BigNumber[]): ContractTxFunctionObj<[BigNumber[], BigNumber[]]>;
+    /**
+     * Sample sell quotes from Uniswap.
+      * @param router Address of the Uniswap Router
+      * @param takerToken Address of the taker token (what to sell).
+      * @param makerToken Address of the maker token (what to buy).
+      * @param takerTokenAmounts Taker token sell amount for each sample.
+     */
+    sampleSellsFromUniswap(router: string, takerToken: string, makerToken: string, takerTokenAmounts: BigNumber[]): ContractTxFunctionObj<[BigNumber[], BigNumber[]]>;
+    sampleSwapFromUniswap(sellToken: string, buyToken: string, bridgeData: string, takerTokenAmount: BigNumber): ContractTxFunctionObj<BigNumber>;
+    constructor(address: string, supportedProvider: SupportedProvider, txDefaults?: Partial<TxData>, logDecodeDependencies?: {
+        [contractName: string]: ContractAbi;
+    }, deployedBytecode?: string | undefined, encoderOverrides?: Partial<EncoderOverrides>);
+}
+//# sourceMappingURL=uniswap_sampler.d.ts.map
