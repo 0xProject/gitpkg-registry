a4e6ae8e509dad08237954fd75eed7de6bdd353c
Lawrence Forman <me@merklejerk.com>
2021-07-09 16:37:00 -0400

    gitpkg

---
+++ lib/generated-wrappers/uniswap_v3_sell_sampler.d.ts
@@ -0,0 +1,50 @@
+import { EncoderOverrides, ContractTxFunctionObj, BaseContract } from '@0x/base-contract';
+import { ContractAbi, ContractArtifact, TxData, SupportedProvider } from 'ethereum-types';
+import { BigNumber } from '@0x/utils';
+import { SimpleContractArtifact } from '@0x/types';
+import { Web3Wrapper } from '@0x/web3-wrapper';
+export declare class UniswapV3SellSamplerContract extends BaseContract {
+    /**
+     * @ignore
+     */
+    static deployedBytecode: string | undefined;
+    static contractName: string;
+    private readonly _methodABIIndex;
+    static deployFrom0xArtifactAsync(artifact: ContractArtifact | SimpleContractArtifact, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }): Promise<UniswapV3SellSamplerContract>;
+    static deployWithLibrariesFrom0xArtifactAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }): Promise<UniswapV3SellSamplerContract>;
+    static deployAsync(bytecode: string, abi: ContractAbi, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: ContractAbi;
+    }): Promise<UniswapV3SellSamplerContract>;
+    /**
+     * @returns      The contract ABI
+     */
+    static ABI(): ContractAbi;
+    protected static _deployLibrariesAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, web3Wrapper: Web3Wrapper, txDefaults: Partial<TxData>, libraryAddresses?: {
+        [libraryName: string]: string;
+    }): Promise<{
+        [libraryName: string]: string;
+    }>;
+    getFunctionSignature(methodName: string): string;
+    getABIDecodedTransactionData<T>(methodName: string, callData: string): T;
+    getABIDecodedReturnData<T>(methodName: string, callData: string): T;
+    getSelector(methodName: string): string;
+    /**
+     * Sample sell quotes from UniswapV3.
+      * @param quoter UniswapV3 Quoter contract.
+      * @param path Token route. Should be takerToken -> makerToken
+      * @param takerTokenAmounts Taker token sell amount for each sample.
+     */
+    sampleSells(quoter: string, path: string[], takerTokenAmounts: BigNumber[]): ContractTxFunctionObj<[string[], BigNumber[]]>;
+    constructor(address: string, supportedProvider: SupportedProvider, txDefaults?: Partial<TxData>, logDecodeDependencies?: {
+        [contractName: string]: ContractAbi;
+    }, deployedBytecode?: string | undefined, encoderOverrides?: Partial<EncoderOverrides>);
+}
+//# sourceMappingURL=uniswap_v3_sell_sampler.d.ts.map
---
+++ lib/src/artifacts.d.ts
@@ -0,0 +1,12 @@
+import { ContractArtifact } from 'ethereum-types';
+export declare const artifacts: {
+    ERC20BridgeSampler: ContractArtifact;
+    BalanceChecker: ContractArtifact;
+    FakeTaker: ContractArtifact;
+    CallDispatcher: ContractArtifact;
+    UniswapV3SellSampler: ContractArtifact;
+    UniswapV3BuySampler: ContractArtifact;
+    NetworkUtils: ContractArtifact;
+    NativeOrderUtils: ContractArtifact;
+};
+//# sourceMappingURL=artifacts.d.ts.map
