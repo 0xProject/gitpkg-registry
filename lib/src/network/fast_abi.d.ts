7c90c26e15eb95bab156a0e28adbde0d4a39d7ab
Lawrence Forman <me@merklejerk.com>
2021-07-20 16:50:48 -0400

    gitpkg

---
+++ lib/src/network/fast_abi.d.ts
@@ -0,0 +1,4 @@
+import { EncoderOverrides } from '@0x/base-contract';
+import { ContractWrapperType } from './types';
+export declare function createFastAbiEncoderOverrides<T extends any>(contractType: ContractWrapperType<T>): EncoderOverrides;
+//# sourceMappingURL=fast_abi.d.ts.map
