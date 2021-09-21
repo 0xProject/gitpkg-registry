7f70ca413ab9b8d63fa3da65fac58ae72161675f
Kim Persson <kimpersson88@gmail.com>
2021-09-21 22:39:23 +0200

    gitpkg

---
+++ lib/src/utils/market_operation_utils/path.d.ts
@@ -0,0 +1,57 @@
+import { BigNumber } from '@0x/utils';
+import { MarketOperation } from '../../types';
+import { CreateOrderFromPathOpts } from './orders';
+import { CollapsedFill, ExchangeProxyOverhead, Fill, OptimizedMarketOrder } from './types';
+export interface PathSize {
+    input: BigNumber;
+    output: BigNumber;
+}
+export interface PathPenaltyOpts {
+    outputAmountPerEth: BigNumber;
+    inputAmountPerEth: BigNumber;
+    exchangeProxyOverhead: ExchangeProxyOverhead;
+    gasPrice: BigNumber;
+}
+export declare const DEFAULT_PATH_PENALTY_OPTS: PathPenaltyOpts;
+export declare class Path {
+    protected readonly side: MarketOperation;
+    fills: ReadonlyArray<Fill>;
+    protected readonly targetInput: BigNumber;
+    readonly pathPenaltyOpts: PathPenaltyOpts;
+    collapsedFills?: ReadonlyArray<CollapsedFill>;
+    orders?: OptimizedMarketOrder[];
+    sourceFlags: bigint;
+    protected _size: PathSize;
+    protected _adjustedSize: PathSize;
+    static create(side: MarketOperation, fills: ReadonlyArray<Fill>, targetInput?: BigNumber, pathPenaltyOpts?: PathPenaltyOpts): Path;
+    static clone(base: Path): Path;
+    protected constructor(side: MarketOperation, fills: ReadonlyArray<Fill>, targetInput: BigNumber, pathPenaltyOpts: PathPenaltyOpts);
+    append(fill: Fill): this;
+    /**
+     * Add a fallback path to the current path
+     * Fallback must contain exclusive fills that are
+     * not present in this path
+     */
+    addFallback(fallback: Path): this;
+    collapse(opts: CreateOrderFromPathOpts): CollapsedPath;
+    size(): PathSize;
+    adjustedSize(): PathSize;
+    adjustedCompleteRate(): BigNumber;
+    adjustedRate(): BigNumber;
+    /**
+     * Returns the best possible rate this path can offer, given the fills.
+     */
+    bestRate(): BigNumber;
+    adjustedSlippage(maxRate: BigNumber): number;
+    isBetterThan(other: Path): boolean;
+    isComplete(): boolean;
+    isValid(skipDuplicateCheck?: boolean): boolean;
+    isValidNextFill(fill: Fill): boolean;
+    collapseFills(): ReadonlyArray<CollapsedFill>;
+    private _addFillSize;
+}
+export interface CollapsedPath extends Path {
+    readonly collapsedFills: ReadonlyArray<CollapsedFill>;
+    readonly orders: OptimizedMarketOrder[];
+}
+//# sourceMappingURL=path.d.ts.map
