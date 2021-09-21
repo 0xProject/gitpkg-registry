9b78d85b99f3d0bcaf6239ddaeb9b8808249783b
Kim Persson <kimpersson88@gmail.com>
2021-09-21 18:06:28 +0200

    gitpkg

---
+++ lib/src/utils/market_operation_utils/fills.d.ts
@@ -0,0 +1,19 @@
+import { BigNumber } from '@0x/utils';
+import { MarketOperation, NativeOrderWithFillableAmounts } from '../../types';
+import { DexSample, ERC20BridgeSource, FeeSchedule, Fill } from './types';
+/**
+ * Create `Fill` objects from orders and dex quotes.
+ */
+export declare function createFills(opts: {
+    side: MarketOperation;
+    orders?: NativeOrderWithFillableAmounts[];
+    dexQuotes?: DexSample[][];
+    targetInput?: BigNumber;
+    outputAmountPerEth?: BigNumber;
+    inputAmountPerEth?: BigNumber;
+    excludedSources?: ERC20BridgeSource[];
+    feeSchedule?: FeeSchedule;
+}): Fill[][];
+export declare function nativeOrdersToFills(side: MarketOperation, orders: NativeOrderWithFillableAmounts[], targetInput: BigNumber | undefined, outputAmountPerEth: BigNumber, inputAmountPerEth: BigNumber, fees: FeeSchedule): Fill[];
+export declare function dexSamplesToFills(side: MarketOperation, samples: DexSample[], outputAmountPerEth: BigNumber, inputAmountPerEth: BigNumber, fees: FeeSchedule): Fill[];
+//# sourceMappingURL=fills.d.ts.map
