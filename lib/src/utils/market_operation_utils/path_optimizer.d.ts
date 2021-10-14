edddeefd93c99e48ddad35042321ae9f0f6128bd
Kim Persson <kimpersson88@gmail.com>
2021-10-14 16:07:53 +0200

    gitpkg

---
+++ lib/src/utils/market_operation_utils/path_optimizer.d.ts
@@ -0,0 +1,14 @@
+import { ChainId } from '@0x/contract-addresses';
+import { BigNumber } from '@0x/utils';
+import { MarketOperation, NativeOrderWithFillableAmounts } from '../../types';
+import { Path, PathPenaltyOpts } from './path';
+import { DexSample, FeeSchedule, Fill } from './types';
+export declare function findOptimalRustPathFromSamples(side: MarketOperation, samples: DexSample[][], nativeOrders: NativeOrderWithFillableAmounts[], input: BigNumber, opts: PathPenaltyOpts, fees: FeeSchedule, chainId: ChainId): Path | undefined;
+/**
+ * Find the optimal mixture of fills that maximizes (for sells) or minimizes
+ * (for buys) output, while meeting the input requirement.
+ */
+export declare function findOptimalPathJSAsync(side: MarketOperation, fills: Fill[][], targetInput: BigNumber, runLimit?: number, opts?: PathPenaltyOpts): Promise<Path | undefined>;
+export declare function fillsToSortedPaths(fills: Fill[][], side: MarketOperation, targetInput: BigNumber, opts: PathPenaltyOpts): Path[];
+export declare function reducePaths(sortedPaths: Path[], side: MarketOperation): Path[];
+//# sourceMappingURL=path_optimizer.d.ts.map
