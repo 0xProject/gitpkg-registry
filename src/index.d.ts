4c91a0708e4831a3f8d01988e1e6da2532ba0ac7
Kim Persson <kimpersson88@gmail.com>
2022-02-24 20:25:18 +0100

    gitpkg

---
+++ src/index.d.ts
@@ -0,0 +1,20 @@
+export interface OptimizerCapture {
+  side: 'Sell' | 'Buy';
+  targetInput: number; // Amount to sell/buy
+  pathsIn: SerializedPath[]; // Input samples
+  pathOut?: SerializedPath;
+}
+
+export interface SerializedPath {
+  ids: string[]; // unique ID for each sample, used to reconstruct the result in TS.
+  inputs: number[]; // sample inputs (taker amount for sells, maker amounts for buys)
+  outputs: number[]; // sample outputs (maker amounts for buys, taker amounts for sells)
+  outputFees: number[]; // fees denominated in output token
+}
+
+export declare function route(
+  source: OptimizerCapture,
+  strategy: Float64Array,
+  output_amounts: Float64Array,
+  numSamples?: number, // Number of samples after interpolation. Defaults to 200 if not provided
+): undefined;
