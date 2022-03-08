6f72fdc79c97868c86f2e57d1acccc90e2a94dea
Lawrence Forman <me@merklejerk.com>
2022-03-08 15:42:10 -0500

    gitpkg

---
+++ lib/src/artifacts.d.ts
@@ -0,0 +1,6 @@
+import { ContractArtifact } from 'ethereum-types';
+export declare const artifacts: {
+    BalanceChecker: ContractArtifact;
+    FakeTaker: ContractArtifact;
+};
+//# sourceMappingURL=artifacts.d.ts.map
---
+++ lib/src/wrappers.d.ts
@@ -0,0 +1,3 @@
+export * from '../generated-wrappers/balance_checker';
+export * from '../generated-wrappers/fake_taker';
+//# sourceMappingURL=wrappers.d.ts.map
