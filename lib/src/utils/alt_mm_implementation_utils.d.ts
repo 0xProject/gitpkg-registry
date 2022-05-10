2904e295c818b3a68de7b39839af927865e66453
Kyu Hyun Chang <kyuhyun217@gmail.com>
2022-05-10 09:11:35 -0700

    gitpkg

---
+++ lib/src/utils/alt_mm_implementation_utils.d.ts
@@ -0,0 +1,16 @@
+import { TakerRequestQueryParamsUnnested } from '@0x/quote-server';
+import { AxiosInstance, CancelToken } from 'axios';
+import { AltOffering, AltQuoteModel, AltRfqMakerAssetOfferings, LogFunction } from '../types';
+/**
+ * Returns the AltOffering if it exists for a given pair
+ */
+export declare function getAltMarketInfo(offerings: AltOffering[], buyTokenAddress: string, sellTokenAddress: string): AltOffering | undefined;
+/**
+ * Turn a standard quote request into an alt quote request
+ * and return the appropriate standard quote response
+ */
+export declare function returnQuoteFromAltMMAsync<ResponseT>(url: string, apiKey: string, profile: string, integratorKey: string, quoteModel: AltQuoteModel, makerToken: string, takerToken: string, maxResponseTimeMs: number, altRfqAssetOfferings: AltRfqMakerAssetOfferings, takerRequestQueryParams: TakerRequestQueryParamsUnnested, axiosInstance: AxiosInstance, warningLogger: LogFunction, cancelToken: CancelToken): Promise<{
+    data: ResponseT;
+    status: number;
+}>;
+//# sourceMappingURL=alt_mm_implementation_utils.d.ts.map
