d8f0929531e5bad101b0ccf4ceca026321f853c3
Jacob Evans <jacob@dekz.net>
2022-05-19 12:26:21 +1000

    gitpkg

---
+++ lib/src/utils/market_operation_utils/identity_fill_adjustor.d.ts
@@ -0,0 +1,6 @@
+import { BigNumber } from '@0x/utils';
+import { Fill, FillAdjustor } from './types';
+export declare class IdentityFillAdjustor implements FillAdjustor {
+    adjustFills(fills: Fill[], amount: BigNumber): Fill[];
+}
+//# sourceMappingURL=identity_fill_adjustor.d.ts.map
