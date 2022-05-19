d44dfe4a2336fa2aa0f874fdd68f0dd193bec7b7
Jacob Evans <jacob@dekz.net>
2022-05-19 11:57:19 +1000

    gitpkg

---
+++ lib/src/utils/rfq_client_mappers.d.ts
@@ -0,0 +1,7 @@
+import { SignedNativeOrder } from '../types';
+import { RfqClientV1Quote } from './irfq_client';
+/**
+ * Converts a RfqClientRfqOrderFirmQuote to a SignedNativeOrder
+ */
+export declare const toSignedNativeOrder: (quote: RfqClientV1Quote) => SignedNativeOrder;
+//# sourceMappingURL=rfq_client_mappers.d.ts.map
